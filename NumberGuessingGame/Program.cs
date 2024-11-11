3bf493ca13f34b01160f022cc5db55485c83989c
Enrico Brosque Palma <enricob.palma@gmail.com>
2024-11-11 14:30:30 -0300

    Initial Commit

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Welcome to the Number Guesser Game!");
+Console.Write("I'm thinking of a number from 1 to 10: ");
+
+var userChosenNumber = int.Parse(Console.ReadLine());
+
+var randomNumber = new Random().Next(1, 11);
+
+if (userChosenNumber == randomNumber)
+{
+    Console.WriteLine("Congratulations! You have guessed the correct number 🎉");
+    Console.WriteLine($"Guessed number: {userChosenNumber}");
+}
+else
+{
+    Console.WriteLine($"Oops, wrong number, i was thinking of the number {randomNumber}");
+}
+
